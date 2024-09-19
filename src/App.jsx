bd3fe7a7fdfc4207b112e3dc7ff74bbd5ee0afff
Mukesh Rauniyar <mukeshrauniyar.me@gmail.com>
2024-09-19 15:43:12 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Manager from './components/manager'
+import Footer from './components/Footer'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar />
+    <div className="bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+    <Manager/>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
